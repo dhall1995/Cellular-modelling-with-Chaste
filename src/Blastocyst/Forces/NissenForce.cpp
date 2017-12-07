26d339a2a29528501343c6c7cb0fb21397933eda
dhall1995 <33090471+dhall1995@users.noreply.github.com>
2017-12-07 13:15:01 +0000

    Update NissenForce.cpp

--- src/Blastocyst/Forces/NissenForce.cpp
+++ src/Blastocyst/Forces/NissenForce.cpp
@@ -101,7 +101,7 @@ c_vector<double, SPACE_DIM> NissenForce<ELEMENT_DIM,SPACE_DIM>::CalculateForceBe
             // No cells should ever interact beyond the cutoff length
             if (this->mUseCutOffLength)
             {
-                if (d/2.0 >= this->GetCutOffLength())  //remember chaste distances given in DIAMETERS
+                if (d >= this->GetCutOffLength())  //remember chaste distances given in DIAMETERS
                 {
                     return force;
                 }
@@ -113,9 +113,6 @@ c_vector<double, SPACE_DIM> NissenForce<ELEMENT_DIM,SPACE_DIM>::CalculateForceBe
             * CELL 'BETWEEN' THEM WITHIN THE INTERACTION DISTANCE I.E. FOR CELLS A AND B THERE DOES NOT EXIST A CELL C 
             * SUCH THAT (DISTANCE_FROM_A_TO_C)
             */
-          
-            potential_gradient = exp(-d/10.0)*unit_vector_from_A_to_B/5.0;
-            potential_gradient_repulsion = -exp(-d/2.0)*unit_vector_from_A_to_B;
             
             // Fill vectors using the polarity_vector data which should be stored when specifiying trophectoderm (See TestNodeBasedMorula.hpp)
             CellPolaritySrnModel* p_srn_model_A = static_cast<CellPolaritySrnModel*>(p_cell_A->GetSrnModel());
