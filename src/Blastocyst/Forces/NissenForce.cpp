fd3f470292ca902bc8c670f3aad215b0e1dedf0f
dhall1995 <33090471+dhall1995@users.noreply.github.com>
2017-12-07 12:57:30 +0000

    Update NissenForce.cpp

--- src/Blastocyst/Forces/NissenForce.cpp
+++ src/Blastocyst/Forces/NissenForce.cpp
@@ -123,7 +123,7 @@ c_vector<double, SPACE_DIM> NissenForce<ELEMENT_DIM,SPACE_DIM>::CalculateForceBe
          
             double cell_difference_angle = atan2(unit_vector_from_A_to_B[1],unit_vector_from_A_to_B[0]);
           
-            double polarity_factor = -sin(cell_difference_angle - angle_A)*sin(cell_difference_angle - angle_B);
+            double polarity_factor = sin(cell_difference_angle - angle_A)*sin(cell_difference_angle - angle_B);
           
             double s = mS_TE_TE;
             
