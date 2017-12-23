0a37a89caa0a14786d7c73a90baed8ea6b08ad02
dhall1995 <33090471+dhall1995@users.noreply.github.com>
2017-12-23 21:06:46 +0000

    Update NissenForceTrophectoderm.cpp

--- src/Blastocyst/Forces/NissenForceTrophectoderm.cpp
+++ src/Blastocyst/Forces/NissenForceTrophectoderm.cpp
@@ -325,10 +325,11 @@ c_vector<double, SPACE_DIM> NissenForceTrophectoderm<ELEMENT_DIM,SPACE_DIM>::Cal
             }
  
        }
+       /*
        //CASE 1-3: Cell B is Undetermined ICM
        else if(p_cell_B->GetCellProliferativeType()->template IsType<TransitCellProliferativeType>())
        {
-            /*
+            
             TRACE("calculating TE-ICM interaction");
           
             //Initialise the distances between the focii and the centre of cell B
@@ -389,8 +390,9 @@ c_vector<double, SPACE_DIM> NissenForceTrophectoderm<ELEMENT_DIM,SPACE_DIM>::Cal
                force = (force_first_A_focus_B + force_second_A_focus_B)/number_of_active_forces;
                return force;
             }
-*/
+
        }
+       */
        //CASE 1-4: Cell B is Primitive Endoderm
        else if(p_cell_B->GetCellProliferativeType()->template IsType<PrECellProliferativeType>())
        {          
