905956f64373585e35db2cb8bff5c68f97df7353
dhall1995 <33090471+dhall1995@users.noreply.github.com>
2017-12-30 20:42:52 +0000

    Update NissenForceTrophectoderm.cpp

--- src/Blastocyst/Forces/NissenForceTrophectoderm.cpp
+++ src/Blastocyst/Forces/NissenForceTrophectoderm.cpp
@@ -15,7 +15,7 @@ NissenForceTrophectoderm<ELEMENT_DIM,SPACE_DIM>::NissenForceTrophectoderm()
      mS_TE_EPI(0.6),  // TE-EPI interaction strength
      mS_TE_PrE(0.4),  // TE-PrE interaction strength
      mS_TE_TE(-1.4),  // TE-TE interaction strength - NOTE: This is just a prefactor and polarity effects will be included
-     mGrowthDuration(3.0)
+     mGrowthDuration(2.0)
 {
 }
 
@@ -138,6 +138,8 @@ c_vector<double, SPACE_DIM> NissenForceTrophectoderm<ELEMENT_DIM,SPACE_DIM>::Cal
             //if each of the cells is young then we treat them as spherical with radius 2.0 whilst they grow
             if (ageA < mGrowthDuration && ageB < mGrowthDuration)
             {
+               s /= 2.0;
+               
                double cell_difference_angle = atan2(unit_vector_from_A_to_B[1],unit_vector_from_A_to_B[0]);
                double polarity_factor = -sin(cell_difference_angle - angle_A)*sin(cell_difference_angle - angle_B);
                
